ce750491b68051642480e9318cdec0c54c49f0c3
Kevin Ishmael <kevinishmael91@gmail.com>
2024-02-17 03:07:07 -0500

    created a function to create divs

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
-function mskeDivs(){}
+/// Selects the container to put divs in
+const container = document.querySelector('#gridContainer')
 
 
 
 
+/// function to create divs
+function createDivs(numDivs) {
 
+    var parentElement = document.getElementById("gridContainer");
 
+   
+    for (var i = 0; i < numDivs; i++) {
+       
+        var newDiv = document.createElement("div");
 
+        
+       
 
-
-
-
-
-
-
-for(let i = 0; i < 16; i++){
-
-
-
-
-
-
-
-
+        
+        parentElement.appendChild(newDiv);
+    }
 }
+
+ 
+createDivs(7);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
